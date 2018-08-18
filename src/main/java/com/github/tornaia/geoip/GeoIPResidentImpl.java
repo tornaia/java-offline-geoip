253fd77ef9229681bde539871d1a63880613379d
Tornai András, PMK-ACS-SWA <andras.tornai@swisscom.com>
2018-08-18 23:24:30 +0200

    Replace double-checked locking Use an inner static class to hold the reference instead. Inner static classes are guaranteed to load lazily.

--- src/main/java/com/github/tornaia/geoip/GeoIPResidentImpl.java
+++ src/main/java/com/github/tornaia/geoip/GeoIPResidentImpl.java
@@ -19,7 +19,13 @@ import static com.github.tornaia.geoip.GeoLiteConstants.LOCATIONS_CSV_GEO_ID;
 
 public class GeoIPResidentImpl implements GeoIP {
 
-    private Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap;
+    private static class ResourceHolder {
+        private static final Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap = getCidrNotationsToCountryIdoCodeMap();
+    }
+
+    private static Map<IpAddressMatcher, String> getCidrNotationsToCountryIsoCodeMap() {
+        return GeoIPResidentImpl.ResourceHolder.cidrNotationsToCountryIsoCodeMap;
+    }
 
     @Override
     public Optional<String> getCountryIsoCode(InetAddress inetAddress) {
@@ -28,9 +34,7 @@ public class GeoIPResidentImpl implements GeoIP {
 
     @Override
     public Optional<String> getCountryIsoCode(String ipAddress) {
-        initIfNecessary();
-
-        return cidrNotationsToCountryIsoCodeMap
+        return getCidrNotationsToCountryIsoCodeMap()
                 .entrySet()
                 .parallelStream()
                 .filter(e -> e.getKey().matches(ipAddress))
@@ -39,45 +43,40 @@ public class GeoIPResidentImpl implements GeoIP {
                 .findFirst();
     }
 
-    private void initIfNecessary() {
-        if (cidrNotationsToCountryIsoCodeMap == null) {
-            synchronized (this) {
-                if (cidrNotationsToCountryIsoCodeMap == null) {
-                    cidrNotationsToCountryIsoCodeMap = new HashMap<>();
-                    Map<String, String> countryIdToCountryIsoCodeMap = getCountryIdToCountryIsoCodeMap();
-                    loadIpv4Csv(countryIdToCountryIsoCodeMap);
-                    loadIpv6Csv(countryIdToCountryIsoCodeMap);
-                }
-            }
-        }
+    private static Map<IpAddressMatcher, String> getCidrNotationsToCountryIdoCodeMap() {
+        Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap = new HashMap<>();
+        Map<String, String> countryIdToCountryIsoCodeMap = getCountryIdToCountryIsoCodeMap();
+        loadIpv4Csv(cidrNotationsToCountryIsoCodeMap, countryIdToCountryIsoCodeMap);
+        loadIpv6Csv(cidrNotationsToCountryIsoCodeMap, countryIdToCountryIsoCodeMap);
+        return cidrNotationsToCountryIsoCodeMap;
     }
 
-    private void loadIpv4Csv(Map<String, String> countryIdToCountryIsoCodeMap) {
-        loadIpvCsv(countryIdToCountryIsoCodeMap, "GeoLite2-Country-Blocks-IPv4.csv");
+    private static void loadIpv4Csv(Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap, Map<String, String> countryIdToCountryIsoCodeMap) {
+        loadIpvCsv(cidrNotationsToCountryIsoCodeMap, countryIdToCountryIsoCodeMap, "GeoLite2-Country-Blocks-IPv4.csv");
     }
 
-    private void loadIpv6Csv(Map<String, String> countryIdToCountryIsoCodeMap) {
-        loadIpvCsv(countryIdToCountryIsoCodeMap, "GeoLite2-Country-Blocks-IPv6.csv");
+    private static void loadIpv6Csv(Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap, Map<String, String> countryIdToCountryIsoCodeMap) {
+        loadIpvCsv(cidrNotationsToCountryIsoCodeMap, countryIdToCountryIsoCodeMap, "GeoLite2-Country-Blocks-IPv6.csv");
     }
 
-    private void loadIpvCsv(Map<String, String> countryIdToCountryIsoCodeMap, String filename) {
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream(filename)) {
+    private static void loadIpvCsv(Map<IpAddressMatcher, String> cidrNotationsToCountryIsoCodeMap, Map<String, String> countryIdToCountryIsoCodeMap, String filename) {
+        try (InputStream is = GeoIPResidentImpl.class.getClassLoader().getResourceAsStream(filename)) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            Map<IpAddressMatcher, String> ipv6Map = reader
+            Map<IpAddressMatcher, String> ipMap = reader
                     .lines()
                     .parallel()
                     .skip(1)
                     .map(e -> e.split(","))
                     .filter(e -> !e[IPV_CSV_REGISTERED_COUNTRY_GEONAME_ID].isEmpty() || !e[IPV_CSV_GEONAME_ID].isEmpty())
                     .collect(Collectors.toMap(e -> new IpAddressMatcher(e[LOCATIONS_CSV_GEO_ID]), e -> mapCountryCodeToCountryIsoCode(countryIdToCountryIsoCodeMap, e)));
-            cidrNotationsToCountryIsoCodeMap.putAll(ipv6Map);
+            cidrNotationsToCountryIsoCodeMap.putAll(ipMap);
         } catch (IOException e) {
             throw new GeoIPException("Failed to read csv: " + filename, e);
         }
     }
 
-    private Map<String, String> getCountryIdToCountryIsoCodeMap() {
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream("GeoLite2-Country-Locations-en.csv")) {
+    private static Map<String, String> getCountryIdToCountryIsoCodeMap() {
+        try (InputStream is = GeoIPResidentImpl.class.getClassLoader().getResourceAsStream("GeoLite2-Country-Locations-en.csv")) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             return reader
                     .lines()
@@ -90,14 +89,14 @@ public class GeoIPResidentImpl implements GeoIP {
         }
     }
 
-    private String mapCountryCodeToCountryIsoCode(Map<String, String> countryIdToCountryIsoCodeMap, String[] splittedLine) {
+    private static String mapCountryCodeToCountryIsoCode(Map<String, String> countryIdToCountryIsoCodeMap, String[] splittedLine) {
         String registeredCountryGeonameId = splittedLine[IPV_CSV_REGISTERED_COUNTRY_GEONAME_ID];
         String geonameId = splittedLine[IPV_CSV_GEONAME_ID];
         String countryId = registeredCountryGeonameId.isEmpty() ? geonameId : registeredCountryGeonameId;
         return getCountryIsoCodeFromCountryId(countryIdToCountryIsoCodeMap, countryId);
     }
 
-    private String getCountryIsoCodeFromCountryId(Map<String, String> countryIdToCountryIsoCodeMap, String countryId) {
+    private static String getCountryIsoCodeFromCountryId(Map<String, String> countryIdToCountryIsoCodeMap, String countryId) {
         String countryIsoCode = countryIdToCountryIsoCodeMap.get(countryId);
         boolean countryIsoCodeWasNotFoundForCountryId = countryIsoCode == null;
         if (countryIsoCodeWasNotFoundForCountryId) {
