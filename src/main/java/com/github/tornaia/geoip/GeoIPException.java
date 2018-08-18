41fb998c39741f0a2bc52c7415eeea53103b28db
Tornai András, PMK-ACS-SWA <andras.tornai@swisscom.com>
2018-08-18 16:37:31 +0200

    Add missing serialVersionUID for serializable class

--- src/main/java/com/github/tornaia/geoip/GeoIPException.java
+++ src/main/java/com/github/tornaia/geoip/GeoIPException.java
@@ -2,6 +2,8 @@ package com.github.tornaia.geoip;
 
 public class GeoIPException extends RuntimeException {
 
+    private static final long serialVersionUID = -2363078259040227858L;
+
     public GeoIPException(String message) {
         super(message);
     }
