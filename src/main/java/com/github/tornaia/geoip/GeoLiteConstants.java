d2ffb4f6ec998de317233cfb33ec4da356c3e028
Tornai András, PMK-ACS-SWA <andras.tornai@swisscom.com>
2018-08-18 23:27:19 +0200

    Add a private constructor to hide the implicit public one

--- src/main/java/com/github/tornaia/geoip/GeoLiteConstants.java
+++ src/main/java/com/github/tornaia/geoip/GeoLiteConstants.java
@@ -2,6 +2,9 @@ package com.github.tornaia.geoip;
 
 public final class GeoLiteConstants {
 
+    private GeoLiteConstants() {
+    }
+
     public static final int LOCATIONS_CSV_GEO_ID = 0;
     public static final int LOCATIONS_CSV_COUNTRY_CODE_ISO = 4;
 
