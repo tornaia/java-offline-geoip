bda250e1116f6ca055f57ce6b92d133c07b9da08
Tornai András, PMK-ACS-SWA <andras.tornai@swisscom.com>
2018-08-25 09:47:19 +0200

    Support eagerly (default) and lazily intialized resident geoip service

--- src/main/java/com/github/tornaia/geoip/GeoIPProvider.java
+++ src/main/java/com/github/tornaia/geoip/GeoIPProvider.java
@@ -2,14 +2,29 @@ package com.github.tornaia.geoip;
 
 public final class GeoIPProvider {
 
+    public enum Type {
+        RESIDENT, RESIDENT_LAZY, NON_RESIDENT
+    }
+
     private GeoIPProvider() {
     }
 
-    public static GeoIP getNonResidentGeoIP() {
-        return new GeoIPNonResidentImpl();
+    public static GeoIP getGeoIP() {
+        return getGeoIP(Type.RESIDENT);
     }
 
-    public static GeoIP getResidentGeoIP() {
-        return new GeoIPResidentImpl();
+    public static GeoIP getGeoIP(Type type) {
+        switch (type) {
+            case RESIDENT:
+                return new GeoIPResidentImpl();
+            case RESIDENT_LAZY:
+                GeoIPResidentImpl residentGeoIP = new GeoIPResidentImpl();
+                residentGeoIP.getTwoLetterCountryCode("127.0.0.1");
+                return residentGeoIP;
+            case NON_RESIDENT:
+                return new GeoIPNonResidentImpl();
+            default:
+                throw new IllegalStateException("Unexpected type: " + type);
+        }
     }
 }
--- src/test/java/com/github/tornaia/geoip/GeoIPTest.java
+++ src/test/java/com/github/tornaia/geoip/GeoIPTest.java
@@ -14,106 +14,113 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class GeoIPTest {
 
     @Test
+    public void ipv4StringWithDefault() {
+        GeoIP geoIP = GeoIPProvider.getGeoIP();
+        Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("92.104.171.16");
+        assertEquals("CH", optionalCountryIsoCode.get());
+    }
+
+    @Test
     public void ipv4StringWithNonResident() {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("92.104.171.16");
         assertEquals("CH", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv6StringWithNonResident() {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("2001:4860:4860::8888");
         assertEquals("US", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv4InetAddressWithNonResident() throws Exception {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode(InetAddress.getByName("92.104.171.16"));
         assertEquals("CH", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv6InetAddressWithNonResident() throws Exception {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode(InetAddress.getByName("2001:4860:4860::8888"));
         assertEquals("US", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv4StringWithResident() {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("92.104.171.16");
         assertEquals("CH", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv6StringWithResident() throws UnknownHostException {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode(InetAddress.getByName("2001:4860:4860::8888"));
         assertEquals("US", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv4InetAddressWithResident() throws Exception {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode(InetAddress.getByName("92.104.171.16"));
         assertEquals("CH", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipv6InetAddressWithResident() throws Exception {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode(InetAddress.getByName("2001:4860:4860::8888"));
         assertEquals("US", optionalCountryIsoCode.get());
     }
 
     @Test
     public void ipBelongsToEUAndNotToAnyCountryThenReturnOptionalEmptyWithNonResident() {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("2.16.4.0");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void ipBelongsToEUAndNotToAnyCountryThenReturnOptionalEmptyWithResident() {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("2.16.4.0");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void fallbackToGeonameIdWhichIsEUWithNonResident() {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("195.51.217.1");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void fallbackToGeonameIdWhichIsEUWithResident() {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("195.51.217.1");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void noRegisteredCountryAndGeonameIdWithNonResident() {
-        GeoIP geoIP = GeoIPProvider.getNonResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.NON_RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("86.62.5.1");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void noRegisteredCountryAndGeonameIdWithResident() {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
         Optional<String> optionalCountryIsoCode = geoIP.getTwoLetterCountryCode("86.62.5.1");
         assertFalse(optionalCountryIsoCode.isPresent());
     }
 
     @Test
     public void residentReallyInitializesJustOnce() {
-        GeoIP geoIP = GeoIPProvider.getResidentGeoIP();
+        GeoIP geoIP = GeoIPProvider.getGeoIP(GeoIPProvider.Type.RESIDENT);
 
         long start = System.currentTimeMillis();
         for (int i = 0; i < 50; ++i) {
